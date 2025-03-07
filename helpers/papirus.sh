89af4afaf6ccad83b6d2ce90d2d943b0addd9cf0
Michael Schwieger <git@zekurio.xyz>
2025-03-07 23:12:27 +0100

    feat: remove cleanup script and add papirus installation script

--- helpers/papirus.sh
+++ helpers/papirus.sh
@@ -3,9 +3,8 @@
 set -euo pipefail
 
 # set INSTALL_DIR to the skel directory
-SKEL_DIR="/etc/skel"
-ICON_INSTALL_DIR="$SKEL_DIR/.icons"
-PAPIRUS_FOLDERS_INSTALL_DIR="$SKEL_DIR/.local"
+ICON_INSTALL_DIR="$HOME/.icons"
+PAPIRUS_FOLDERS_INSTALL_DIR="$HOME/.local"
 
 mkdir -p "$ICON_INSTALL_DIR" "$PAPIRUS_FOLDERS_INSTALL_DIR"
 
@@ -13,4 +12,4 @@ mkdir -p "$ICON_INSTALL_DIR" "$PAPIRUS_FOLDERS_INSTALL_DIR"
 wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$ICON_INSTALL_DIR" sh
 
 # install papirus-folders
-wget -qO- https://git.io/papirus-folders-install | env PREFIX=$PAPIRUS_FOLDERS_INSTALL_DIR sh
+wget -qO- https://git.io/papirus-folders-install | env PREFIX="$PAPIRUS_FOLDERS_INSTALL_DIR" sh
